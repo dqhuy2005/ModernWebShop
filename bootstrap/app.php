0c9ca9dfa1b4b6f4a382e55021c2e83fd3ef2262
Quốc Huy Đặng <danghuy0751245@gmail.com>
2025-11-25 00:03:08 +0700

    config cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,11 @@ return Application::configure(basePath: dirname(__DIR__))
             return route('login');
         });
 
+        // CORS
+        $middleware->api(prepend: [
+            \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
+
         $middleware->web(append: [
             \App\Http\Middleware\CheckUserStatus::class,
         ]);
